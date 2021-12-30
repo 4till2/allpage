eb7738be2b050f7b92e03e357d167c9d6c978f78
Yosef Serkez <yosefserkez@gmail.com>
2021-12-30 10:06:18 -0500

    home page allpage

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,10 @@ import Router from "next/router";
 import Layout from "../components/layout";
 export default function Home() {
     if (typeof window !== "undefined"){
-        Router.push('/discover')
+        Router.push('/allpage')
         return <></>
     }
   return (
-    <Layout className={"m-auto flex"}>
-        <h1 className={"m-auto p-32 text-center text-6xl font-bold"}>Oops you might be lost.
-            <a href="/discover" className={"underline text-green-800"}> Go Explore!</a>
-        </h1>
-    </Layout>
+  <></>
   )
 }
